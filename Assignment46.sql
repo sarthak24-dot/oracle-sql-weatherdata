94f04ed0059161732bd2609346725590fc726eef
sarthak24-dot <sarthakbasu24@gmail.com>
2024-10-01 14:02:19 -0400

    Update Assignment46.sql

--- Assignment46.sql
+++ Assignment46.sql
@@ -2,18 +2,22 @@ SELECT DateColumn, MaxTemperature, MinTemperature, Thunderstorm, Rainfall
 FROM DailyWeather
 WHERE City = 'Miami' AND MaxTemperature >= 93
 ORDER BY DateColumn;
+
 SELECT DateColumn, MaxTemperature, MinTemperature, Thunderstorm, Rainfall
 FROM DailyWeather
 WHERE City = 'Orlando' AND MaxTemperature >= 93
 ORDER BY DateColumn;
+
 SELECT DateColumn, MaxTemperature, MinTemperature, Thunderstorm, Rainfall
 FROM DailyWeather
 WHERE City = 'Tampa' AND MaxTemperature >= 93
 ORDER BY DateColumn;
+
 SELECT DateColumn, MaxTemperature, MinTemperature, Thunderstorm, Rainfall
 FROM DailyWeather
 WHERE City = 'Indianapolis' AND MaxTemperature >= 93
 ORDER BY DateColumn;
+
 SELECT DateColumn, MaxTemperature, MinTemperature, Thunderstorm, Rainfall
 FROM DailyWeather
 WHERE City = 'New Orleans' AND MaxTemperature >= 93
